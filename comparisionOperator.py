55d6c4b4ce2c565e26b2c3fc05289134562b88ce
IshwarBachhav2611 <ishwarbachhav2611@gmail.com>
2025-12-03 00:40:13 +0530

    [ADDED]:Ishwar this code shows use of Comparision Operators

---
+++ comparisionOperator.py
@@ -0,0 +1,20 @@
+x = 10
+y = 20
+
+# Equal to
+print("x == y:", x == y)
+
+# Not equal to
+print("x != y:", x != y)
+
+# Greater than
+print("x > y:", x > y)
+
+# Less than
+print("x < y:", x < y)
+
+# Greater than or equal to
+print("x >= 10:", x >= 10)
+
+# Less than or equal to
+print("y <= 15:", y <= 15)
