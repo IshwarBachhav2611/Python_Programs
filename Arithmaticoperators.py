00da5e4098f1334cea486bf8456d8ee322843415
IshwarBachhav2611 <ishwarbachhav2611@gmail.com>
2025-12-03 00:39:15 +0530

    [ADDED]:Ishwar this code shows use of Arithmatic Operators

---
+++ Arithmaticoperators.py
@@ -0,0 +1,23 @@
+a = 10
+b = 3
+
+# Addition
+print("Addition:", a + b)
+
+# Subtraction
+print("Subtraction:", a - b)
+
+# Multiplication
+print("Multiplication:", a * b)
+
+# Division (float)
+print("Division:", a / b)
+
+# Floor Division (integer division)
+print("Floor Division:", a // b)
+
+# Modulus (remainder)
+print("Modulus:", a % b)
+
+# Exponent (power)
+print("Exponent:", a ** b)
