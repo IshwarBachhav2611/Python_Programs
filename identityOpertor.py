cdb6d2394a1d611160b26183057b420ab85d55b3
IshwarBachhav2611 <ishwarbachhav2611@gmail.com>
2025-12-04 00:40:46 +0530

    [ADDED] Ishwar: This code shows use of Identity Opertors

---
+++ identityOpertor.py
@@ -0,0 +1,18 @@
+a = 10
+b = 10
+c = [1, 2, 3]
+d = [1, 2, 3]
+
+# 'is' checks if both refer to the same memory location
+print("a is b:", a is b)         
+
+# 'is not' checks if memory locations are different
+print("c is d:", c is d)            
+print("c is not d:", c is not d)  
+
+# Additional example
+x = None
+y = None
+
+print("x is y:", x is y)            
+print("x is not y:", x is not y)    
