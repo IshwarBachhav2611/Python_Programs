714ca6af3d1ec87fd166ac62164f70c3934b951d
IshwarBachhav2611 <ishwarbachhav2611@gmail.com>
2025-12-04 00:39:50 +0530

    [ADDED] Ishwar: This code shows use of Membership Opertors

---
+++ membershipOperator.py
@@ -0,0 +1,19 @@
+# List example
+numbers = [10, 20, 30, 40]
+
+print("20 in numbers:", 20 in numbers)        
+print("50 in numbers:", 50 in numbers)        
+print("50 not in numbers:", 50 not in numbers) 
+
+# String example
+name = "python"
+
+print("'p' in name:", 'p' in name)            
+print("'z' in name:", 'z' in name)            
+print("'on' in name:", 'on' in name)          
+
+# Tuple example
+colors = ("red", "blue", "green")
+
+print("'blue' in colors:", 'blue' in colors)   
+print("'yellow' not in colors:", 'yellow' not in colors) 
